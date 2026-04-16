482c7b94ce7106478ec1dd0fd00924d9fde1a32d
Zubair Hasan <zubairmuhammad476@gmail.com>
2026-04-16 20:46:45 +0500

    fix: allow Hostinger backend in CSP and update production config

--- next.config.ts
+++ next.config.ts
@@ -34,7 +34,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https://ik.imagekit.io https://images.pexels.com",
-              "connect-src 'self' http://localhost:8000 http://127.0.0.1:8000 https://ik.imagekit.io https://upload.imagekit.io",
+              "connect-src 'self' http://localhost:8000 http://127.0.0.1:8000 https://darkviolet-pony-419059.hostingersite.com https://ik.imagekit.io https://upload.imagekit.io",
               "frame-ancestors 'none'",
             ].join("; "),
           },
